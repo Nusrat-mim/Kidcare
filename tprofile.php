b7dc81146d6267b72189501a3c358daafcffd447
nuzhat77r <61469465+nuzhat77r@users.noreply.github.com>
2021-09-11 12:42:49 +0600

    Update tprofile.php

--- tprofile.php
+++ tprofile.php
@@ -7,17 +7,20 @@ $username=$_SESSION['username'];
 $sql="SELECT * FROM parent WHERE username='$username'";
 $result=$conn->query($sql);
 $retrive=mysqli_fetch_array($result);
-
+//print_r($retrive);
 
 $firstname=$retrive['firstname'];
 $lastname=$retrive['lastname'];
 $phone=$retrive['phone'];
 $address=$retrive['address'];
 
-
+// echo "Name: ".$firstname." ".$lastname."<br>";
+// echo "Phone: ".$phone."<br>";
+// echo "Institution: ".$institution."<br>";
+// echo "Address: ".$address."<br>";
+// echo "Username: ".$username;
 
  ?>
-
 
 <!DOCTYPE html>
 <html>
@@ -34,7 +37,6 @@ $address=$retrive['address'];
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="profilestyle.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
 <body>
 	<header>
 		<div class="container">
@@ -43,6 +45,8 @@ $address=$retrive['address'];
 				<ul>	
 					<li><a href="thome.php"><i class="fa fa-home"><br>Home</i></a></li>
 					<li><a href="tprofile.php"><i class="fa fa-user-circle"><br>Profile</i></a></li>
+					<li><a href="findstudent.php"><i class="fa fa-search"><br>Find DayCare</i></a></li>
+					<li><a href="tstatus.php"><i class="fa fa-users"><br>Status</i></a></li>
 					<li><a href="message.php"><i class="fa fa-telegram"><br>Messages</i></a></li>
 					<li><a href="logout.php"><i class="fa fa-sign-out"><br>Logout</i></a></li>
 				</ul>
