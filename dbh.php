8a576acb454b3a28902cf2e149c8299631ca58ce
nuzhat77r <61469465+nuzhat77r@users.noreply.github.com>
2021-09-11 12:50:24 +0600

    Update dbh.php

--- dbh.php
+++ dbh.php
@@ -1,5 +1,5 @@
 <?php
-$conn=mysqli_connect("localhost","root","","kidcaredb");
+$conn=mysqli_connect("localhost","root","","projectdb");
 if(!$conn)
 	die("Connection Failed".mysqli_connect_error());
 
