0c55078e0be7269fef6eea8b1a3cf9af38faf7ce
Nusrat-mim <58785813+Nusrat-mim@users.noreply.github.com>
2021-09-11 18:42:00 +0600

    kidcare bot updated

---
+++ bot.php
@@ -0,0 +1,74 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KIDCARE</title>
+    <link rel="stylesheet" href="bot.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+</head>
+<body>
+<header>
+            
+            
+            <ul class="navigation">
+                <li><a href="index.html"><button class="button">Home</button></a></li>
+                <li><a href="index.php"><button class="button">login</button></a></li>
+                <li><a href="aboutus.html"><button class="button">About Us</button></a></li>
+                <li><a href="FAQ.html"><button class="button">FAQ</button></a></li>
+                <li><a href="Blogs.html"><button class="button">Blogs</button></a></li>
+               
+
+
+
+            </ul>
+            
+            </header>
+    <div class="wrapper">
+        <div class="title">KIDCARE</div>
+        <div class="form">
+            <div class="bot-inbox inbox">
+                <div class="icon">
+                    <i class="fas fa-user"></i>
+                </div>
+                <div class="msg-header">
+                    <p>Hello there, how can I help you?</p>
+                </div>
+            </div>
+        </div>
+        <div class="typing-field">
+            <div class="input-data">
+                <input id="data" type="text" placeholder="Type something here.." required>
+                <button id="send-btn">Send</button>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        $(document).ready(function(){
+            $("#send-btn").on("click", function(){
+                $value = $("#data").val();
+                $msg = '<div class="user-inbox inbox"><div class="msg-header"><p>'+ $value +'</p></div></div>';
+                $(".form").append($msg);
+                $("#data").val('');
+                
+            
+                $.ajax({
+                    url: 'message.php',
+                    type: 'POST',
+                    data: 'text='+$value,
+                    success: function(result){
+                        $replay = '<div class="bot-inbox inbox"><div class="icon"><i class="fas fa-user"></i></div><div class="msg-header"><p>'+ result +'</p></div></div>';
+                        $(".form").append($replay);
+                        
+                        $(".form").scrollTop($(".form")[0].scrollHeight);
+                    }
+                });
+            });
+        });
+    </script>
+    
+</body>
+</html>
