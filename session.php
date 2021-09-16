33c99adcad381f4454905298ebc5c01f6353ba86
KamrunNaher01 <86709366+KamrunNaher01@users.noreply.github.com>
2021-09-16 11:31:07 +0600

    Update session.php

--- session.php
+++ session.php
@@ -5,10 +5,6 @@ include 'dbh.php';
 
 $username=$_SESSION['username'];
 
-// $sql="SELECT * FROM student WHERE username='$username'";
-// $result=$conn->query($sql);
-// $retrive=mysqli_fetch_array($result);
-
 
 
  ?>
